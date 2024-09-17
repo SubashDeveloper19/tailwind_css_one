063e7dfb5d24ca6f03902d2d0b58ed66e9330224
SubashDeveloper19 <sksubash1911@gmail.com>
2024-09-18 01:32:57 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        primary:"#009A74",
+      },
+      fontFamily:{
+        jost:['jost','sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
